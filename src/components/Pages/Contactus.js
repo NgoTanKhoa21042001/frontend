b681381538d8412b475ba9e1c976bc809ae68fc3
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-03-23 10:14:34 +0700

    contact us page

--- src/components/Pages/Contactus.js
+++ src/components/Pages/Contactus.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import "./contact.css";
 const Contact = () => {
-  return <div>Contact us</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0ozn8ic",
+        "template_oz7nxui",
+        form.current,
+        "d7NqazpazxlFem3aX"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast.success("Email sent successfully");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="contact-form">
+      <h1 className="contact-title">Contact Us</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
